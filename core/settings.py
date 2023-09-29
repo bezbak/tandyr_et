16e12202896c0e1a74982a5c632494613eb6f368
bezbak <Cemetey.2019@gmail.com>
2023-09-29 12:12:25 +0600

    delete config

--- core/settings.py
+++ core/settings.py
@@ -1,6 +1,7 @@
 
 from pathlib import Path
 from . import config
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -116,6 +117,10 @@ EMAIL_USE_TLS = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'frontend/build/static'),
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
